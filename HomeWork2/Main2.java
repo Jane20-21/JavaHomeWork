39dbfedf1b5c7f71e0642ac8f6f51c2676cb9e1c
Evgeniya <j.nikif15@gmail.com>
2022-11-27 17:32:27 +0300

    Add Home Work for Seminar2

---
+++ HomeWork2/Main2.java
@@ -0,0 +1,53 @@
+package JavaHomeWork.HomeWork2;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Task task = new Task();
+        task.Print(task.Sort(task.Taskt()));
+    }
+}
+
+class Task {
+    int n;
+    int[] array;
+
+    int[] Taskt() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите размер массива: ");
+        n = input.nextInt();
+        array = new int[n];
+        System.out.println("Введите элементы массива: ");
+        for (int i=0; i<array.length; i++) {
+            array[i] = input.nextInt();
+        }
+        input.close();
+        System.out.println("\n");
+        return array;
+    }
+
+    int[] Sort(int[] array) {  
+        for (int i = 0; i < array.length; i++) {
+            int min = array[i];
+            int minId = i;
+            for (int j = i+1; j < array.length; j++) {
+                if (array[j] < min) {
+                    min = array[j];
+                    minId = j;
+                }
+            }
+            // замена
+            int temp = array[i];
+            array[i] = min;
+            array[minId] = temp;
+        }
+        return array;    
+    }
+
+    void Print(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
