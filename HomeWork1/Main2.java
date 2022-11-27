0c38a8ac042d13c35cb49dd2cbfe098ec5188e59
Evgeniya <j.nikif15@gmail.com>
2022-11-27 15:02:28 +0300

    Add task2

--- HomeWork1/Main2.java
+++ HomeWork1/Main2.java
@@ -2,7 +2,7 @@ package JavaHomeWork.HomeWork1;
 
 public class Main2 {
     public static void main(String[] args) {
-        for (int i=0; i<=1000; i++) {
+        for (int i=1; i<=1000; i++) {
             System.out.println(i);
         }
     }
